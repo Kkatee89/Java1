b62375feba856e078ba293e4127c2d5272d861bc
Kkatee89 <a.katya-89@yandex.ru>
2022-01-28 13:05:04 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         int eaters = 5; // сколько людей будут есть
-
         int water = 3000; // миллилитров воды
         int potatoes = 5; // картофелин
         int chicken = 6; // куриных бёдер
@@ -11,7 +10,7 @@ public class Main {
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров воды");
         System.out.println((potatoes / eaters) + " картофелин(а)");
-        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+       System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ка) специй");
 
     }
